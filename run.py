bc5c9958984fe4508d7c7256a98687b92e6da139
kisakyegordon <gordon56692@gmail.com>
2017-08-21 15:35:05 +0300

    Improved A[[lication

--- run.py
+++ run.py
@@ -1,35 +1,62 @@
 from flask import *
 from functools import wraps
+from app.user import User
 
 app = Flask(__name__)
 
 app.secret_key = 'elgordo123456789'
-
-users = {'admin':'admin','art':'art'}
-new_users = {}
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')
+users = {'admin': 'admin', 'art': 'art'}
+user = User()
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/index')
+def index():
+    lists = user.shopping_lists
+    return render_template('index.html', lists=lists)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
 def register():
     error = None
     if request.method == 'POST':
-        username = request.form['rname']
-        password = request.form['pword']
-        flash("You have succesfully registered {} {}".format(username, password))
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        flash("You have succesfully been registered {} {}".format(name, password))
+
+        if name and password:
+            users[name] = password
+            return redirect(url_for('login'))
+    return render_template('signup.html', error=error)
 
 
-        if username and password:
-            new_users[username] = password
-            return redirect(url_for('log'))
-    return render_template('register.html', error=error)
+@app.route('/add_list', methods=['GET', 'POST'])
+def add_list():
+    error = None
+    if request.method == 'POST':
+        list_name = request.form['list_name']
+        items = request.form['items']
+        flash("You have succesfully added registered {} {}".format(list_name, items))
+
+        if list_name and items:
+            user.create_shopping_list(list_name, items)
+            return redirect(url_for('index'))
+    return render_template('add_list.html', error=error)
+
+
+@app.route('/add_item', methods=['GET', 'POST'])
+def add_item():
+    error = None
+    if request.method == 'POST':
+        list_name = request.form['list_name']
+        items = request.form['items']
+        flash("You have succesfully added registered {} {}".format(list_name, items))
+
+        if list_name and items:
+            user.add_shopping_list_item(list_name, items)
+            return redirect(url_for('item', list_name=list_name))
+    return render_template('add_list.html', error=error)
+
 
 def login_required(test):
     @wraps(test)
@@ -41,32 +68,46 @@ def login_required(test):
             return redirect(url_for('log'))
     return wrap
 
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You Were Logged Out !')
-    return redirect (url_for('log'))
+    return redirect(url_for('login'))
 
-@app.route('/hello')
+
+@app.route('/item/<list_name>')
 @login_required
-def hello():
-    return render_template('hello.html')
+def item(list_name):
+    items = user.read_list(list_name)
+    return render_template('item.html', items=items, list_name=list_name)
 
 
-@app.route('/log', methods=['GET', 'POST'])
-def log():
-    users = {'admin': 'admin', 'art': 'art'}
+@app.route('/delete/<list_name>/<item_name>')
+def delete(list_name, item_name):
+    user.delete_shopping_list_item(list_name, item_name)
+    return redirect(url_for('item', list_name=list_name))
+    return render_template('item.html')
+
+
+@app.route('/delete_list/<list_name>')
+def delete_list(list_name):
+    user.delete_shopping_list(list_name)
+    return redirect(url_for('index'))
+    return render_template('index.html')
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] not in users.keys() or request.form['password'] not in users.values():
+        if request.form['email'] not in users.keys() or request.form['password'] not in users.values():
             error = 'Invalid Credentials'
         else:
             session['logged_in'] = True
-            return redirect(url_for('hello'))
-    return render_template('log.html', error=error)
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
